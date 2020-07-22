f6a6ad2f7023a30c69cc60c33459eb2ecbaac00e
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-22 16:58:31 -0400

    implemented css grid

--- client/src/components/Slot/Slot.js
+++ client/src/components/Slot/Slot.js
@@ -1,11 +1,20 @@
 import React from "react";
+/** Styles **/
+import "./Slot.css";
 /** Helpers **/
 import { dayToWords } from "../../util/formatHelpers";
 
 const Slot = ({ day, hour }) => {
+  const _day = dayToWords(day);
+
   return (
-    <div>
-      <div>{`${dayToWords(day)} ${hour} h`}</div>
+    <div
+      onClick={() => {
+        console.log("You clicked", _day, hour);
+      }}
+      className={`Slot__container ${_day}`}
+    >
+      <div>{`${_day} ${hour} h`}</div>
     </div>
   );
 };
--- client/src/components/WeekView/WeekView.js
+++ client/src/components/WeekView/WeekView.js
@@ -10,17 +10,9 @@ import { DAYS, HOURS } from "../../util/constants";
 
 const WeekView = () => {
   /** Redux **/
-  console.log("WeekView mounting again.");
+  console.log("WeekView mounted.");
+
   // spread slot for rendering
-  useEffect(() => {}, []);
-  let daysJSX = [];
-  let hoursJSX = [];
-  // for (let i = 0; i < DAYS; i++) {
-  //   daysJSX[i] = <div>{`Day ${i}`}</div>;
-  // }
-  // for (let i = 0; i < HOURS; i++) {
-  //   hoursJSX[i] = <div>{`${i} h`}</div>;
-  // }
   const tiles = [];
   for (let i = 0; i < DAYS; i++) {
     for (let j = 0; j < HOURS; j++) {
