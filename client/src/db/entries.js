2f7133c0a376ef438188ffcf59e19d7fb87d1328
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-22 15:16:57 -0400

    setting up dummy data

---
+++ client/src/db/entries.js
@@ -0,0 +1,4 @@
+// entry ID format: wwddtt
+// e.g.  => 1-5-23 (1st week, Thursday, 11pm);
+
+const entries = {};
--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -1,3 +1,12 @@
 /** Redux actions **/
 export const INCREMENT = "increment";
 export const DECREMENT = "decrement";
+
+/** For calendar **/
+const WEEKS = 52;
+const DAYS = 7;
+const HOURS = 24;
+
+/** Data required for entries */
+const DRIVERS = ["Anna", "Bob", "Chris"];
+const TASKS = ["DROP OFF", "PICK UP", "MISC"];
