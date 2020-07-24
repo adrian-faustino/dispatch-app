00377b60f1ab42dcd3e8d5985e78774e422de631
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-24 19:46:40 -0400

    fixed logic on button showing while entry form is open

---
+++ client/src/components/Suggestions/Suggestions.js
@@ -0,0 +1,10 @@
+import React from "react";
+/** Styles **/
+import "./Suggestions.css";
+/** Handlers **/
+
+const Suggestions = () => {
+  return <div>suggestions!</div>;
+};
+
+export default Suggestions;
