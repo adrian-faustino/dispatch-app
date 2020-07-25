0a0159b84e68065dee04f69af92e756409baa4c8
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-25 01:59:57 -0400

    wrote helper function to increase/decrease current date

--- client/src/components/Suggestions/SuggestionsHandlers.js
+++ client/src/components/Suggestions/SuggestionsHandlers.js
@@ -1,6 +1,7 @@
 /** Helpers **/
 import { getOccupiedSlots } from "../../util/dbHelpers";
 import { dateObjToStringID } from "../../util/formatHelpers";
+import { WEEK, DAY, HOUR } from "../../util/constants";
 
 const SuggestionsHandlers = (dispatch, store) => {
   // take current date
@@ -8,12 +9,27 @@ const SuggestionsHandlers = (dispatch, store) => {
   const currentDate = dateObjToStringID(dateObj);
   // get occupied slots
   const occupiedSlots = getOccupiedSlots();
+  console.log("Current date:", currentDate);
+  console.log("Ahead date:", aheadDate(currentDate, WEEK, 3));
 
   // with filtered db, check slots for
   for (let date of occupiedSlots) {
-    console.log("Date:", date);
+    const [week, day, hour] = date.split("-");
   }
   // later today (1 hour or before)
+  const calcWithinHour = () => {
+    const
+    const result = ['ahead', 'behind'];
+
+    result.map(type => {
+      switch(type) {
+        case 'ahead':
+          if ()
+          return
+      }
+    })
+  }
+
   // return +/- 1 hour
   // const calcWithinHour = () => {
   //   const currentHour = dateObj.hour;
@@ -28,3 +44,17 @@ const SuggestionsHandlers = (dispatch, store) => {
 };
 
 export default SuggestionsHandlers;
+
+// function that takes a date string and increases the 'week' or 'day' or 'hour' - return string
+function aheadDate(dateStr, selector, delta) {
+  const [week, day, hour] = dateStr.split("-").map((e) => parseInt(e));
+
+  switch (selector) {
+    case WEEK:
+      return `${week + delta}-${day}-${hour}`;
+    case DAY:
+      return `${week}-${day + delta}-${hour}`;
+    case HOUR:
+      return `${week}-${day}-${hour + delta}`;
+  }
+}
