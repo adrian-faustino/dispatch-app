c791c87d19dd1a8c2d6db6e2adede5646b7aba50
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-26 15:36:29 -0400

    added css constants

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 /** Subcomponents **/
 import NavControllers from "./NavControllers";
+/** Styles **/
+import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div>
-      <div>navbar</div>
+    <div className="Nav__navbar">
+      <div className="Nav__site-logo">navbar</div>
+      <btn className="Nav__toggle-btn"></btn>
 
-      <NavControllers />
+      <div className="Nav__li-container">
+        <NavControllers />
+      </div>
     </div>
   );
 };
--- client/src/components/Nav/NavControllers.js
+++ client/src/components/Nav/NavControllers.js
@@ -1,4 +1,6 @@
 import React from "react";
+/** Styles **/
+import "./Nav.css";
 
 const NavControllers = () => {
   return <div>nav controller</div>;
