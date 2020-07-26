298b8efe4efece535761c23092be6abccaa6b528
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-26 15:27:22 -0400

    Added navbar

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+/** Subcomponents **/
+import NavControllers from "./NavControllers";
+
+const Nav = () => {
+  return (
+    <div>
+      <div>navbar</div>
+
+      <NavControllers />
+    </div>
+  );
+};
+
+export default Nav;
---
+++ client/src/components/Nav/NavControllers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavControllers = () => {
+  return <div>nav controller</div>;
+};
+
+export default NavControllers;
