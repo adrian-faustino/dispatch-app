675d0adc1150b566d72143a2efe0bb4473c5ddee
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-26 21:07:10 -0400

    schedule styling

--- client/src/components/Slot/SlotView/SlotView.js
+++ client/src/components/Slot/SlotView/SlotView.js
@@ -1,16 +1,8 @@
 import React from "react";
-/** Helpers **/
-import { dayToWords } from "../../../util/formatHelpers";
 
 const SlotView = ({ dateObj, bookedData }) => {
-  /** Variables **/
-  const { week, day, hour } = dateObj;
-  const _day = dayToWords(day);
-
   return (
     <div>
-      <div>{`${_day} ${hour} h`}</div>
-
       {bookedData && (
         <div>
           <div>Driver: {bookedData.driver}</div>
