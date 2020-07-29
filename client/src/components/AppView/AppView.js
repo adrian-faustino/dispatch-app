28d4781f796d667a9c76b4a0f8f8abe743fb21b0
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-07-29 17:48:04 -0400

    refactored error prompt

--- client/src/components/AppView/AppView.js
+++ client/src/components/AppView/AppView.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 /** Subcomponents **/
 import ErrorPrompt from "../ErrorPrompt/ErrorPrompt";
 /** Redux **/
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { resetError } from "../../actions/errorActions";
 /** Styles **/
 import "./AppView.css";
-/** Constants **/
-import { SCHEDULE_VIEW } from "../../util/constants";
 
 function AppView() {
   const store = useSelector((state) => state);
-  const date = useSelector((state) => state.date);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // close any error prompts whenever view changes
+    handleCloseError();
+  }, [store.appView]);
+
+  const handleCloseError = () => {
+    dispatch(resetError());
+  };
+
+  // move position of background depending on slider
+  const errorStyle = {
+    left: store.slideInToggled && "var(--slide-in-width)",
+  };
   return (
     <div className="AppView__container">
-      {/* Span displaying week */}
-      {/* {store.appView === SCHEDULE_VIEW && (
-        <div className="AppView__week-span medium-text">Week {date.week}</div>
-      )} */}
-
       {/* Prompt displaying input errors */}
       {store.error.errMsg && <ErrorPrompt />}
+
+      {/* Area around error prompt */}
+      {store.error.errMsg && (
+        <div
+          onClick={handleCloseError}
+          style={errorStyle}
+          className="App__error-dismiss-area"
+        ></div>
+      )}
     </div>
   );
 }
